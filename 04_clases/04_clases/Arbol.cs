43a60d9db8fa38d76a3caaa007867d0a1a0327a7
GJParts <39961515+gjparts@users.noreply.github.com>
2026-01-28 10:51:12 -0600

    commit 2801202630657

---
+++ 04_clases/04_clases/Arbol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_clases
+{
+    /*reemplazamos internal por public en la definicion
+     de la clase para que esta sera accesible
+    desde cualquier proyecto dentro de nuestra
+    solucion.
+    Para nombrar clases y miembros en C# se recomienda
+    usar la notacion Pascal Case.*/
+    public class Arbol
+    {
+        /*Propiedades
+         Son las caracteristicas de la clase, la describen.*/
+        public string Nombre { get; set; } //auto-implemented property
+        public string Especie { get; set; }
+        public float Altura { get; set; }
+        public int Edad {  get; set; }
+        /*Metodos
+         Son las funciones dentro de la clase.*/
+        public void Purificar()
+        {
+            Console.WriteLine("Soy un arbol y estoy purificando el oxigeno");
+        }
+    }
+}
---
+++ 04_clases/04_clases/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _04_clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*en C# para nombrar variables y
+             constantes locales se recomienda
+            usar camel Case o Snake Case, ejemplos:
+            Camel Case o notacion Hungara: (primera letra minusc. demas en mayusc)
+            arbol1
+            cuentaAhorro1
+            salarioEmpleadoEmpresa46
+
+            Snake Case: (todo en minusculas, espacios son guion bajo)
+            arbol1
+            cuenta_ahorro1
+            salario_empleado_empresa46
+            */
+
+            //instanciar un objeto de clase Arbol
+            Arbol a1 = new Arbol();
+            //leer (get) algunas propiedades de a1
+            Console.WriteLine(a1.Edad);
+            Console.WriteLine(a1.Especie);
+            //escribir (set) datos a las propiedades de a1:
+            a1.Edad = 100;
+            a1.Especie = "Ceiba Penthandra";
+            a1.Altura = 6.3f;
+            a1.Nombre = "El palon de la esquina";
+            //volver a leer las propiedades actualizadas de a1:
+            Console.WriteLine("Propiedades de a1:");
+            Console.WriteLine($"Nombre: {a1.Nombre}");
+            Console.WriteLine($"Edad: {a1.Edad}");
+            Console.WriteLine($"Especie: {a1.Especie}");
+            Console.WriteLine($"Altura: {a1.Altura}");
+            //ejecucion del unico metodo que le pusimos al Arbol:
+            a1.Purificar();
+        }
+    }
+}
