fb6002fcb00b520c3e6ecad8e7a54882799c0a24
GJParts <39961515+gjparts@users.noreply.github.com>
2026-02-04 10:53:47 -0600

    commit 040220267341

---
+++ 03_Captura1/03_Captura1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _03_Captura1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Captura de valores usando la consola del sistema:
+            //para capturar valores se usa ReadLine()
+            //el cual captura un valor desde la consola
+            //y lo devuelve como cadena de texto (string)
+
+            //bloque de caza de errores en tiempo de ejecucion (runtime)
+            try
+            {
+                //codigo propenso a fallar
+                int a, b;
+                Console.Write("Digite un numero entero: ");
+                a = int.Parse( Console.ReadLine() );
+                Console.Write("Digite otro numero entero: ");
+                b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"La suma de ambos numeros es {a+b}");
+            }catch(Exception ex)
+            {
+                //codigo a ejecutar en caso de falla
+                Console.WriteLine("Debe digitar solo numeros enteros");
+                //Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("************************");
+                double x, y;
+                Console.Write("Digite un numero con decimales: ");
+                x = double.Parse( Console.ReadLine() );
+                Console.Write("Digite otro numero con decimales: ");
+                y = double.Parse(Console.ReadLine());
+                Console.WriteLine($"La resta de ambos numeros es {x-y}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Solo se acepta numeros");
+            }
+        }
+    }
+}
